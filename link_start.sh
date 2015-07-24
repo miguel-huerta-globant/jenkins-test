76ce266e341c1e1a815abad0a427ed7a79101603
hgmiguel <hgmiguel@gmail.com>
2015-07-24 11:39:12 -0500

    Primeros pasos para automatizar la instalacion

--- link_start.sh
+++ link_start.sh
@@ -1,2 +1,15 @@
-ln -s $HOME/.vim/tmux.conf .tmux.conf
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install vim
+brew install fish
+brew install wget
+
+git clone git@github.com:hgmiguel/dotvim.git $HOME/.vim
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+git clone git@github.com:altercation/solarized.git
+
+ln -s $HOME/.vim/config.fish  $HOME/.config/fish/config.fish
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s $HOME/.vim/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/.vim/gitconfig $HOME/.gitconfig
+
