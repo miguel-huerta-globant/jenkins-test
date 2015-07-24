04fa5358719e07790128e6f5bd1f4e01ee31ce9a
hgmiguel <hgmiguel@gmail.com>
2015-07-24 09:48:45 -0500

    se añaden gvm paths

--- config.fish
+++ config.fish
@@ -1,14 +1,6 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Theme
-set fish_theme coffeeandcode
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins vi-mode gvm tmux brew
-
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 #
@@ -18,5 +10,15 @@ set vi_mode_default vi_mode_normal
 set -x DOCKER_HOST tcp://192.168.59.103:2375
 set --universal fish_user_paths /usr/local/bin $fish_user_paths
 
+
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+Theme "coffeeandcode"
+
+Plugin "vi-mode" 
+Plugin "gvm" 
+Plugin "tmux" 
+Plugin "brew"
+
+set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
