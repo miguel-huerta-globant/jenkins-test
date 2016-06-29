1b02bb894592b18ebe807fd93ca052f284bff335
hgmiguel <hgmiguel@gmail.com>
2016-06-28 19:54:47 -0500

    Algunas actualizaciones

--- config.fish
+++ config.fish
@@ -1,24 +1,21 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH "$HOME/.local/share/omf"
+
+set -gx JAVA_HOME "/usr/lib/jvm/java-7-openjdk-amd64"
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/home/hgmiguel/.config/omf"
+
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 #
 set --export EDITOR "vim -f"
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+#set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 set vi_mode_default vi_mode_normal
-set -x DOCKER_HOST tcp://192.168.59.103:2375
-set --universal fish_user_paths /usr/local/bin $fish_user_paths
-
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+source $OMF_PATH/init.fish
 
-Theme "coffeeandcode"
-
-Plugin "vi-mode" 
-Plugin "gvm" 
-Plugin "tmux" 
-Plugin "brew"
+alias tmux "env TERM=screen-256color-bce tmux"
 
 set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
